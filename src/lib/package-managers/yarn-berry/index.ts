cb9849c6ffb5c34c954674b7e70284e63991b944
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-10-31 12:02:42 +0100

    fix: yarn berry does not have the flag

--- src/lib/package-managers/yarn-berry/index.ts
+++ src/lib/package-managers/yarn-berry/index.ts
@@ -65,9 +65,6 @@ export class YarnBerry extends PackageManager {
 			if (options.exact) {
 				args.push("--exact");
 			}
-			if (options.force) {
-				args.push("--force");
-			}
 		} else {
 			args.push("install");
 			// if (this.environment === "production") {
